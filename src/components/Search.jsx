0f48da3d2ea0c5acd70c77745f4f2871b10479c5
Tray Keyes <trayton26@gmail.com>
2024-04-25 16:32:38 -0700

    api setup and tested for search by character

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,8 @@
 
 import React, { useState } from "react";
 import md5 from 'md5';
+import Characters  from "./Characters";
+import Comics from "./Comics";
 
 export default function Search() {
 
@@ -9,13 +11,14 @@ export default function Search() {
     const [comicData, setComicData] = useState(null);
 
 
-    const publicKey = import.meta.env.VITE_PUBLIC_KEY;
-    const privateKey = import.meta.env.VITE_PRIVATE_KEY;
+    const publicKey ='test';
+    const privateKey = 'test';
     
 
     const getCharacterData = () => {
         setCharacterData(null);
         setComicData(null);
+
         const timeStamp = new Date().getTime();
         const hash = generateHash(timeStamp);
        
@@ -30,6 +33,24 @@ export default function Search() {
         )
         
     }
+
+    const getComicData = (characterId) =>{
+        window.scrollTo({top: 0, left: 0});
+
+        const timeStamp = new Date().getTime();
+        const hash = generateHash(timeStamp);
+
+        const url = `https://gateway.marvel.com:443/v1/public/characters/${characterId}/comics?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}&limit=100`;        
+
+       fetch(url)
+        .then((response) => response.json()).then((results) => {
+            setComicData(results.data);
+            console.log(results)
+        })
+        .catch(error => {console.log("error while passing in the comic data", error)
+    })
+    }
+
     const generateHash = (timeStamp) => {
         return md5(timeStamp +privateKey + publicKey );
     }
@@ -44,9 +65,16 @@ export default function Search() {
 
     const handleChange = (event) =>{
         setSearch(event.target.value);
+        const timeStamp = new Date().getTime();
+        const hash = generateHash(timeStamp);
        
     }
-
+    const handleReset = (event) => {
+        setSearch("")
+        setCharacterData(null)
+        setComicData(null)
+    }
+  
         return (
             <>
                 <form className="search" onSubmit={handleSubmit}>
@@ -55,11 +83,17 @@ export default function Search() {
                     type="text"
                     onChange={handleChange}
                     /> 
+                    <div> 
+                    <button className="submit--button" onClick={handleSubmit}>Find Comics</button>
+                    <button className="resetButton" onClick={handleReset}>Reset</button>
+                </div>
                 </form>
 
-                <div> 
-                    <button className="submit--button" onClick={handleSubmit}>Find Comics</button>
-                </div>
+                {!comicData && characterData && characterData.results[0] &&
+                <Characters data={characterData.results} onClick={getComicData} />
+                }
+                {comicData && comicData.results[0] && <Comics data={comicData.results}/>}
+                
             </>
             )
 }
